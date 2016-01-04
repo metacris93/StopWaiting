cd9d4d08f84201c2a70d5abc90bb1aa5be33164a
Cristian <metacris93@github.com>
2016-01-04 00:59:17 -0500

    agregue las plantillas de login y registro, ademas agregue una plantilla de bienvenido que consiste en una animacion

---
+++ app/src/main/java/com/tutorial/stopwaiting/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.tutorial.stopwaiting;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    private TextView registro_link;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        registro_link = (TextView)findViewById(R.id.registroActivity);
+    }
+
+    public void clickEnlaceRegistro(View view){
+        Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+        startActivity(intent);
+    }
+}
